fd7ac59ed92d06bfd89aa7201a0ccb976b63e0e9
Tang Zhixiong <dvorak4tzx@gmail.com>
2016-05-11 11:12:13 +0800

    works on Windows & Linux now

--- FindFiles.h
+++ FindFiles.h
@@ -4,31 +4,20 @@
 
 #ifndef FIND_FILES_H
 #define FIND_FILES_H
-#pragma once
 
-#include <Windows.h>
-#include<iostream>
-#include <fstream>
 #include <vector>
 #include <string>
-#include <direct.h>
-#include <stdlib.h>  
-#include <stdio.h> 
-
-using namespace std;
 
 class FindFiles
 {
 public:
-	FindFiles(){}
-	~FindFiles(){}
+    FindFiles(){}
+    ~FindFiles(){}
 
-public:
-	std::vector<std::string> file_lists;
+    std::vector<std::string> findFiles( const char *lpPath, const char *secName = ".*" );
 
-public:
-	std::vector<std::string> findFiles(char* lpPath, char* secName = ".*");
-
+private:
+    std::vector<std::string> file_lists;
 };
 
 #endif // FIND_FILES_H
